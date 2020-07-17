62bacfcf91fa6be6b8c1f9101f972da415990ddf
Khang90800 <khang90800@gmail.com>
2020-07-17 14:46:20 +0200

    NC Details - Update mode added

--- src/list-menu/nc-list/nc-details/nc-details.component.ts
+++ src/list-menu/nc-list/nc-details/nc-details.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NcService } from '../../../app/services/nc.service';
 import { ActivatedRoute } from '@angular/router';
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-nc-details',
   templateUrl: './nc-details.component.html',
@@ -9,12 +10,26 @@ import { ActivatedRoute } from '@angular/router';
 export class NcDetailsComponent {
 
   ncId: number = 0;
-  ncLabel: string = 'nc_label';  
+  ncLabel: string = 'nc_label';
+  isModifying = false;
+  test: string = '';
 
   constructor(private ncService: NcService,
               private route: ActivatedRoute) {
     const id = this.route.snapshot.params['id']; 
     this.ncId = this.ncService.getNcById(+id).id;    
     this.ncLabel = this.ncService.getNcById(+id).label;    
+  }
+
+  switchModifyMode(){
+    if(this.isModifying === true) {
+      this.isModifying = false;
+    } else if(this.isModifying === false) {
+      this.isModifying = true;
+    }
+  }
+
+  onSaveChanges() {
+    this.test = 'Test ok !';
   }
 }
